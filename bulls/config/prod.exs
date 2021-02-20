42621ab2b530e868891ed64060c2172b7cfb61af
gigiali <ali.je@husky.neu.edu>
2021-02-20 01:46:25 -0500

    changed port in prod.exs back to 80

--- bulls/config/prod.exs
+++ bulls/config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :bulls, BullsWeb.Endpoint,
-  url: [host: "hw05.jehaneali.site", port: 81],
+  url: [host: "hw05.jehaneali.site", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
