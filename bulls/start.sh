e55daa948ee97a1628e6a3d69cd9ba4163c2ce96
Jehane Ali <51268667+jehaneali@users.noreply.github.com>
2021-03-02 02:54:03 -0500

    Update start.sh

--- bulls/start.sh
+++ bulls/start.sh
@@ -15,11 +15,11 @@ export PORT=4795
 
 echo "Stopping old copy of app, if any..."
 
-_build/dev/rel/bulls/bin/bulls stop || true
+_build/prod/rel/bulls/bin/bulls || true
 
 echo "Starting app..."
 
-_build/dev/rel/bulls/bin/bulls start
+_build/prod/rel/bulls/bin/bulls start
 
 # TODO: Add a systemd service file
 #       to start your app on system boot.
